2ad6a6b59a021e96b5331cdc1ee58cb182484376
Jonhy Olivas <jonhy.olivaspadilla@plexus.tech>
2026-06-14 15:56:47 +0200

    Ajustado el menú.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,6 @@
   import { afterNavigate } from '$app/navigation';
 
   const { children } = $props();
-  let ventasAbierto = $state(false);
-
-  function toggleVentas() { ventasAbierto = !ventasAbierto; }
-
-  afterNavigate(() => { ventasAbierto = false;});
 </script>
 
 <header>
@@ -19,17 +14,7 @@
       <li><a href="/products">Productos</a></li>
       <li><a href="/clients">Clientes</a></li>
       <li><a href="/categories">Categorías</a></li>
-      <li class="has-submenu">
-        <button type="button" class="menu-button" onclick={toggleVentas}>
-          Ventas
-        </button>
-        {#if ventasAbierto}
-          <ul class="submenu">
-            <li><a href="/sales/selling">Vender</a></li>
-            <li><a href="/sales/all">Ver todas</a></li>
-          </ul>
-        {/if}
-      </li>
+      <li><a href="/selling">Vender</a></li>
       <li><a href="/invoices">Facturas</a></li>
     </ul>
   </nav>
@@ -53,8 +38,7 @@
 
   .menu { display: flex; }
 
-  .menu a,
-  .menu-button {
+  .menu a {
     background: none;
     border: none;
     color: #333;
@@ -65,23 +49,8 @@
     text-decoration: none;
   }
 
-  .menu a:hover,
-  .menu-button:hover {
+  .menu a:hover {
     background-color: #333;
     color: #FFF;
   }
-
-  .has-submenu { position: relative; }
-
-  .submenu {
-    border: 1px solid #BBB;
-    left: 0;
-    min-width: 6em;
-    padding: 0;
-    position: absolute;
-    top: 100%;
-    z-index: 1;
-  }
-
-  .submenu a { padding: .5em; }
 </style>
